9c23f3f1e01043a7d84738ef38c9f2dde24f7b55
Md Azharuddin <87271343+Md-Azharuddin02@users.noreply.github.com>
2023-05-18 18:57:43 +0530

    commit

---
+++ script.js
@@ -0,0 +1,93 @@
+// ---------------togle buttom----------------------
+let menuIcon = document.querySelector('#menu-icon');
+let navbar= document.querySelector('.navbar');
+
+menuIcon.onclick = () =>{
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
+
+
+
+
+
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a')
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id= sec.getAttribute('id');
+
+        if(top >= offset &&  top < offset + height){
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        };
+    });
+    let header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 100);
+
+
+    // ------------------------remove toggle------------
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+};
+// -- ----------------------scroll reveal----------------->
+
+
+    ScrollReveal({
+        reset: true,
+        distance: '80px',
+        duration: 2000,
+        delay: 200
+    });
+
+    ScrollReveal().reveal('.home-content , .heading ,.about-content' , {origin:'top'});
+    
+    ScrollReveal().reveal('.home img, .services-container ,.portfolio-box, .contact form' ,  {origin:'bottom'});
+
+    ScrollReveal().reveal('.home-content h1 , .about img' , {origin:'left'});
+    
+    ScrollReveal().reveal('.home-content p , ' , {origin:'top'});
+
+
+    // -----------------------tyed js-----------
+
+    const tyed = new Typed('.multiple-text',{
+        strings:['Web Developer','Java Developer', 'Blogger'],
+        typeSpeed:100,
+        backSpeed:100,
+        backDelay:1000,
+        loop:true
+    });
+
+
+    const tyed2 = new Typed('.multiple-text2',{
+        strings:['Web Developer','Java Developer', 'Cloud Computing'],
+        typeSpeed:100,
+        backSpeed:100,
+        backDelay:1000,
+        loop:true
+    });
+
+    // -------------------------read message-----------------
+    function sendEmail(){
+        Email.send({
+        Host : "smtp.elasticemail.com",
+        Username : "stark155332@gmail.com",
+        Password : "A8CFA9720C30599602ABEE5D602D4AB7F188",
+        To : 'stark155332@gmail.com',
+        From : document.getElementById('email').value,
+        Subject : "New Contact Form Enquiry",
+        Body : "Name:"+ document.getElementById('name').value
+            + "<br> Email:" + document.getElementById('email').value
+            + "<br> Phone no:" + document.getElementById('mobile').value
+            + "<br> Message:" + document.getElementById('text').value
+    }).then(
+    message => alert("Message sent successfully")
+    );
+    }
