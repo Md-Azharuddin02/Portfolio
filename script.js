c2a5b408f75499e98251a2b2139b8a38bc6432ce
Md Azharuddin <mdazharuddin155332@email.com>
2023-05-20 14:58:27 +0530

    commit

--- script.js
+++ script.js
@@ -8,16 +8,13 @@ menuIcon.onclick = () =>{
 };
 
 
-
-
-
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a')
 
 window.onscroll = () => {
     sections.forEach(sec => {
         let top = window.scrollY;
-        let offset = sec.offsetTop - 250;
+        let offset = sec.offsetTop - 150;
         let height = sec.offsetHeight;
         let id= sec.getAttribute('id');
 
