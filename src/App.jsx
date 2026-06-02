7f93f416960a215462167da86d8a94b5d57775c7
Md-Azharuddin02 <mdazharuddin155332@gmail.com>
2026-06-02 12:51:06 +0530

    fix vercel analytics import

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import { Routes, Route } from 'react-router-dom';
 import ErrorBoundary from './Components/ErrorBoundary'
 import Loader from './Components/Home/Loader';
 import PageNotFound from './Components/PageNotFound';
+import { Analytics } from "@vercel/analytics/next"
+
 
 // Lazy load components
 const Layout = React.lazy(() => import('./Components/Layout/Layout'));
@@ -12,13 +14,14 @@ const Home = React.lazy(() => import('./Pages/Home'));
 function App() {
   return (
     <ErrorBoundary>
-      <Suspense fallback={<Loader/>}>
+      <Suspense fallback={<Loader />}>
         <Routes>
           <Route path="/" element={<Layout />}>
             <Route index element={<Home />} />
           </Route>
-            <Route path ="*" element={<PageNotFound />} />
+          <Route path="*" element={<PageNotFound />} />
         </Routes>
+        <Analytics />
       </Suspense>
     </ErrorBoundary>
   );
